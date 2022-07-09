7cb4be402c7411b81079f352ad7e0ec423f2bcf0
Rafael Cena <rafael.cena@hotmail.com>
2022-07-09 08:46:05 -0300

    get customer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
 const express = require('express');
+const {v4: uuidv4} = require('uuid')
 
 const app = express();
+app.use(express.json());
+
+const customers =[]
+
+app.post("/account", (req, res) => {
+    const {cpf,name} = req.body;
+
+    const customerAlreadyExists = customers.find(customer => customer.cpf === cpf);
+
+    if(customerAlreadyExists){
+        return res.status(400).json({error: "Customer already exists"});
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4() ,
+        statement:[]
+    })
+
+    return res.status(201).send()
+
+
+})
+
+app.get('/statement/:cpf', (req, res) => {
+    const {cpf} = req.params;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer){
+        return res.status(400).json({error: "Customer not found"});
+    }
+
+    return res.status(200).json(customer.statement)
+    
+})
+
 
 app.listen(3333, () => {
     console.log('Server started on port 3333');
